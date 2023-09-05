f9e555a5e5db918aeed983dcc97d0c798c8b6326
Evgenii Popov <saip0904@gmail.com>
2023-09-05 08:51:38 +0500

    Задание 13

--- Homework_Lesson2/Homework_013/Program.cs
+++ Homework_Lesson2/Homework_013/Program.cs
@@ -7,8 +7,10 @@
 Console.WriteLine("Напишите трехзначное число");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
+
 if (stringNumber.Length > 2)
 {
     Console.WriteLine($"Третьяя цифра числа {stringNumber[2]}");
 }
+
 else Console.WriteLine ("Нет третьей цифры");

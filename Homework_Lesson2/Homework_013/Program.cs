cae92d6d55c4da9c337d59de8c0ff9021a594b60
Evgenii Popov <saip0904@gmail.com>
2023-09-05 08:38:55 +0500

    Урок 2. Базовые алгоритмы

---
+++ Homework_Lesson2/Homework_013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 123456789 -> 3
+
+Console.WriteLine("Напишите трехзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+if (stringNumber.Length > 2)
+{
+    Console.WriteLine($"Третьяя цифра числа {stringNumber[2]}");
+}
+else Console.WriteLine ("Нет третьей цифры");

33fc6592f5af40e9ae1e2c9e25ec5f24bdcfd64f
Evgenii Popov <saip0904@gmail.com>
2023-09-19 10:30:25 +0500

    Задание 25

---
+++ Homework_Lesson4/Homework_025/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int degree = a;
+
+for (int i = 1; i < b; i++)
+{
+    degree =  a * degree;
+}
+Console.WriteLine($"число {degree}");

3472a17726ead8216d9de2dc12431927874640fc
Evgenii Popov <saip0904@gmail.com>
2023-09-20 09:27:45 +0500

    Задание 29

---
+++ Homework_Lesson4/Homework_029/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        // Создаем массив из 8 элементов типа int 
+        int[] numbers = new int[8]; 
+ 
+        for (int i = 0; i < numbers.Length; i++) 
+        { 
+            numbers[i] = i + 1; 
+        } 
+ 
+        Console.WriteLine("Элементы массива:"); 
+        foreach (int number in numbers) 
+        { 
+            Console.WriteLine(number); 
+        } 
+        Console.ReadKey(); 
+    } 
+}
